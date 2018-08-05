5645892b4ec833114d9a4ae5dcb1e55d63cbd33c
Abdullah muhammed <eng.abodacs@gmail.com>
2018-08-05 21:59:21 +0200

    fix responsive bug and sw.js file

--- README.md
+++ README.md
@@ -21,15 +21,12 @@ These instructions will get you a copy of the project up and running on your loc
 1.  Download the [server](https://github.com/abodacs/mws-restaurant-stage-2)
 
 2.  Download this repo.
-3.  install the dependencies
 
-```shell
-npm -i
-```
+3.  Run the command `cd dist` to change into the `dist` folder and run a local http server.
 
-4.  Start server by running `npm run start:dev` on root folder of server.
+4.  In this folder, start up a simple HTTP server to serve up the site files on your local computer. Python has some simple tools to do this, and you don't even need to know Python. For most people, it's already installed on your computer.
 
-- then, build the app in production mode: `prestart:prod` on project root folder.
+In a terminal, check the version of Python you have: `python -V`. If you have Python 2.x, spin up the server with `python -m SimpleHTTPServer 8000` (or some other port, if port 8000 is already in use.) For Python 3.x, you can use `python3 -m http.server 8000`. If you don't have Python installed, navigate to Python's [website](https://www.python.org/) to download and install the software.
 
 5.  Navigate to `localhost:8000` to view the app.
 
--- src/sw.js
+++ src/sw.js
@@ -1,4 +1,4 @@
-const cacheVersion = 'mws-restaurant-stage1-v11';
+const cacheVersion = 'mws-restaurant-stage1-v16';
 
 const cacheResources = async () => {
   const urlsToCache = [
@@ -6,11 +6,12 @@ const cacheResources = async () => {
     './index.html',
     './restaurant.html',
     'img/logo.svg',
-    '/img/loading_image.svg',
+    'img/loading_image.svg',
     'img/image_not_available.png',
-    'dist/manifest.json',
-    '//unpkg.com/leaflet@1.3.3/dist/leaflet.js',
-    '//unpkg.com/leaflet@1.3.3/dist/leaflet.css'
+    'css/bundle.min.css',
+    '/js/index.js',
+    '/js/restaurant_info.js',
+    'manifest.json'
   ];
   const cache = await caches.open(cacheVersion);
   return cache.addAll(urlsToCache);
@@ -33,6 +34,7 @@ self.addEventListener('activate', (event) => {
 self.addEventListener('fetch', (event) => {
   // Let the browser do its default thing
   // for non-GET requests.
+  console.log('fetch', event.request.url);
   if (event.request.method != 'GET') return;
   const url = new URL(event.request.url);
   let req = event.request;
@@ -45,18 +47,32 @@ self.addEventListener('fetch', (event) => {
   }
   // Prevent the default, and handle the request ourselves.
   event.respondWith(
-    caches.match(req)
-      .then(response => response || fetch(req)
-        .then(response => caches.open(cacheVersion)
-          .then((cache) => {
-            cache.put(event.request.url, response.clone()); // save the response for future
-            return response; // return the fetched data
-          }))
-
-        // Offline fallback
-
-        .catch(() => {
-          return new Response('<svg role="img" aria-labelledby="offline-title" viewBox="0 0 400 300" xmlns="http://www.w3.org/2000/svg"><title id="offline-title">Offline</title><g fill="none" fill-rule="evenodd"><path fill="#D8D8D8" d="M0 0h400v300H0z"/><text fill="#9B9B9B" font-family="Helvetica Neue,Arial,Helvetica,sans-serif" font-size="72" font-weight="bold"><tspan x="93" y="172">offline</tspan></text></g></svg>', { headers: { 'Content-Type': 'image/svg+xml' } });
-        })),
+    cacheThenNetwork(cacheVersion, req)
   );
 });
+
+// Checks the cache first or returns the network
+// response if not found
+const cacheThenNetwork = (cacheName, request) => {
+  // Open the cache
+  return caches.open(cacheName)
+    .then(cache => {
+      // Look for the requested resource in the cache
+      return cache.match(request.url, { ignoreSearch: true })
+        .then(cachedResponse => {
+          // Return the cached response if found
+          // otherwise fetch from the network
+          return cachedResponse || fetch(request)
+            .then(networkResponse => {
+              cache.put(request, networkResponse.clone());
+              // Add the network response to the cache and return it
+              return networkResponse;
+            })
+            .catch(() => {
+              // Offline fallback
+              return new Response('<svg role="img" aria-labelledby="offline-title" viewBox="0 0 400 300" xmlns="http://www.w3.org/2000/svg"><title id="offline-title">Offline</title><g fill="none" fill-rule="evenodd"><path fill="#D8D8D8" d="M0 0h400v300H0z"/><text fill="#9B9B9B" font-family="Helvetica Neue,Arial,Helvetica,sans-serif" font-size="72" font-weight="bold"><tspan x="93" y="172">offline</tspan></text></g></svg>', { headers: { 'Content-Type': 'image/svg+xml' } });
+            });
+          }
+        )}
+      );
+}
